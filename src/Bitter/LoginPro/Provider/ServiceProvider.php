c93b2e2e82c9d58d1398134fcdbf1c553b19ba29
Fabian Bitter <fabian@bitter.de>
2025-07-15 17:57:34 +0200

    Update

--- src/Bitter/LoginPro/Provider/ServiceProvider.php
+++ src/Bitter/LoginPro/Provider/ServiceProvider.php
@@ -4,25 +4,31 @@ namespace Bitter\LoginPro\Provider;
 
 use Concrete\Core\Application\Application;
 use Concrete\Core\Foundation\Service\Provider;
+use Concrete\Core\Html\Service\Html;
 use Concrete\Core\Page\Theme\ThemeRouteCollection;
 use Concrete\Core\Routing\RouterInterface;
 use Bitter\LoginPro\Routing\RouteList;
+use Concrete\Core\View\View;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class ServiceProvider extends Provider
 {
     protected RouterInterface $router;
     protected ThemeRouteCollection $themeRouteCollection;
+    protected EventDispatcherInterface $eventDispatcher;
 
     public function __construct(
-        Application          $app,
-        RouterInterface      $router,
-        ThemeRouteCollection $themeRouteCollection
+        Application              $app,
+        RouterInterface          $router,
+        ThemeRouteCollection     $themeRouteCollection,
+        EventDispatcherInterface $eventDispatcher
     )
     {
         parent::__construct($app);
 
         $this->router = $router;
         $this->themeRouteCollection = $themeRouteCollection;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function register()
@@ -45,6 +51,19 @@ class ServiceProvider extends Provider
 
         $this->themeRouteCollection->setThemeByRoute('/register', 'login_pro');
         $this->themeRouteCollection->setThemeByRoute('/login', 'login_pro');
+
+        // Workaround for 9.4
+        if (defined('APP_VERSION')) {
+            if (version_compare(APP_VERSION, '9.4.0', '>=')) {
+                $this->eventDispatcher->addListener("on_before_render", function () {
+                    $v = View::getInstance();
+                    /** @var Html $htmlService */
+                    /** @noinspection PhpMultipleClassDeclarationsInspection */
+                    $htmlService = $this->app->make(Html::class);
+                    $v->addHeaderAsset($htmlService->css("css/skins/default.css", "login_pro"));
+                });
+            }
+        }
     }
 
     private function registerRoutes()
